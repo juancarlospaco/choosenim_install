0d7296244f2cdca08a59464dc0c8f0159891247c
Juan Carlos <JuanCarlospaco@gmail.com>
2020-08-11 19:52:11 -0300

    update

--- dist/setup.py
+++ dist/setup.py
@@ -3,15 +3,9 @@ import os, pathlib, warnings, setuptools, subprocess, shutil
 
 os.environ["CHOOSENIM_NO_ANALYTICS"] = "1"
 if sys.platform.startswith("win"):
-  for dll_file in os.listdir(pathlib.Path(__file__).parent):
-    if dll_file.endswith(".dll"):
-      try:
-        shutil.copyfile(dll_file, str( pathlib.Path(r"C:\Windows\System32") / pathlib.Path(dll_file).name ))
-        subprocess.run(f"regsvr32 /s { pathlib.Path(r"C:\Windows\System32") / pathlib.Path(dll_file).name }", shell=True, check=False)
-      except:
-        print(f"Error: Failed to install DLL {dll_file}")
-  if subprocess.run(f"{ pathlib.Path(__file__).parent / 'choosenim.exe' } --yes --verbose --noColor --firstInstall", shell=True, check=True, timeout=999).returncode == 0:
-    subprocess.run('nimble.exe -y --noColor refresh'), shell=True, check=False, timeout=999)
+  shutil.rmtree(str(pathlib.Path.home() / ".choosenim"), ignore_errors=True)
+  if subprocess.run(f"{ pathlib.Path(__file__).parent / 'choosenim.exe' } --yes --verbose --noColor --firstInstall stable", shell=True, check=True, timeout=9999).returncode == 0:
+    subprocess.run("nimble.exe -y --noColor refresh", shell=True, check=False, timeout=999)
 else:
   if subprocess.run(f"sh { pathlib.Path(__file__).parent / 'init.sh' } -y", shell=True, check=True, timeout=999).returncode == 0:
     if pathlib.Path(pathlib.Path.home() / ".nimble/bin").exists() and subprocess.run(str(pathlib.Path.home() / '.nimble/bin/nimble -y --noColor refresh'), shell=True, check=True, timeout=999).returncode == 0:
