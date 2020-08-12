f1f589bd47ab11439117ea5aa676203d9ce9a0ab
Juan Carlos <JuanCarlospaco@gmail.com>
2020-08-11 22:44:45 -0300

    -

--- dist/setup.py
+++ dist/setup.py
@@ -8,49 +8,69 @@ shutil.rmtree(str(pathlib.Path.home() / ".nimble"), ignore_errors=True)
 
 if sys.platform.startswith("win"):
   if subprocess.run(f"{ pathlib.Path(__file__).parent / 'choosenim.exe' } --yes --verbose --noColor --firstInstall stable", shell=True, check=True, timeout=9999).returncode == 0:
-    shutil.rmtree(str(pathlib.Path.home() / r".choosenim\downloads"), ignore_errors=True)
+    shutil.rmtree(str(pathlib.Path.home() / ".choosenim" / "downloads"), ignore_errors=True)
     subprocess.run(f"{ pathlib.Path.home() / '.nimble' / 'bin' / 'nimble.exe' } --yes --verbose --noColor refresh", shell=True, check=False, timeout=999)
 else:
   if subprocess.run(f"sh { pathlib.Path(__file__).parent / 'init.sh' } -y", shell=True, check=True, timeout=999).returncode == 0:
     if pathlib.Path(pathlib.Path.home() / ".nimble/bin").exists() and subprocess.run(str(pathlib.Path.home() / '.nimble/bin/nimble -y --noColor refresh'), shell=True, check=True, timeout=999).returncode == 0:
       new_path = f"export PATH={ pathlib.Path.home() / '.nimble/bin' }:$PATH"
+      filename = pathlib.Path.home() / ".bashrc"
       try:
-        found = False
-        with open(pathlib.Path.home() / ".bashrc", "a") as f:
-          for line in f:
-            if new_path == line:
-              found = True
-          if not found:
+        if filename.exists():
+          found = False
+          with open(filename, "a") as f:
+            for line in f:
+              if new_path == line:
+                found = True
+            if not found:
+              f.write(new_path)
+        else:
+          with open(filename, "w") as f:
             f.write(new_path)
       except:
-        warnings.warn("Failed to write file ~/.bashrc")
+        warnings.warn("Failed to write file: " + filename)
+      filename = pathlib.Path.home() / ".profile"
       try:
-        found = False
-        with open(pathlib.Path.home() / ".profile", "a") as f:
-          for line in f:
-            if new_path == line:
-              found = True
-          if not found:
+        if filename.exists():
+          found = False
+          with open(filename, "a") as f:
+            for line in f:
+              if new_path == line:
+                found = True
+            if not found:
+              f.write(new_path)
+        else:
+          with open(filename, "w") as f:
             f.write(new_path)
       except:
         warnings.warn("Failed to write file ~/.profile")
+      filename = pathlib.Path.home() / ".bash_profile"
       try:
-        found = False
-        with open(pathlib.Path.home() / ".bash_profile", "a") as f:
-          for line in f:
-            if new_path == line:
-              found = True
-          if not found:
+        if filename.exists():
+          found = False
+          with open(filename, "a") as f:
+            for line in f:
+              if new_path == line:
+                found = True
+            if not found:
+              f.write(new_path)
+        else:
+          with open(filename, "w") as f:
             f.write(new_path)
       except:
         warnings.warn("Failed to write file ~/.bash_profile")
+      filename = pathlib.Path.home() / ".zshrc"
       try:
-        found = False
-        with open(pathlib.Path.home() / ".zshrc", "a") as f:
-          for line in f:
-            if new_path == line:
-              found = True
-          if not found:
+        if filename.exists():
+          found = False
+          with open(filename, "a") as f:
+            for line in f:
+              if new_path == line:
+                found = True
+            if not found:
+              f.write(new_path)
+        else:
+          with open(filename, "w") as f:
             f.write(new_path)
       except:
         warnings.warn("Failed to write file ~/.zshrc")
