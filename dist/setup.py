e0165290940e0253598494a1364d1b636daef314
Juan Carlos <juancarlospaco@gmail.com>
2021-07-07 15:33:21 -0300

    Python 3.0 compatible

--- dist/setup.py
+++ dist/setup.py
@@ -1,9 +1,38 @@
-import os, sys, pathlib, warnings, setuptools, subprocess, shutil
+import os, sys, warnings, setuptools, subprocess, shutil
 from setuptools.command.install import install
 
 
-assert sys.version_info > (3, 5, 0), "ERROR: Python version must be > 3.5!." # F-String.
+assert sys.version_info > (3, 0, 0), "ERROR: Python version must be > 3.0!."
 os.environ["CHOOSENIM_NO_ANALYTICS"] = "1"
+home = os.path.expanduser("~")
+
+
+def which(cmd, mode = os.F_OK | os.X_OK, path = None):
+  # shutil.which is Python 3.3+ only.
+  def _access_check(fn, mode):
+    return (os.path.exists(fn) and os.access(fn, mode) and not os.path.isdir(fn))
+
+  if _access_check(cmd, mode):
+    return cmd
+  path = (path or os.environ.get("PATH", os.defpath)).split(os.pathsep)
+  if sys.platform == "win32":
+    if os.curdir not in path:
+      path.insert(0, os.curdir)
+    pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+    matches = [cmd for ext in pathext if cmd.lower().endswith(ext.lower())]
+    files = [cmd] if matches else [cmd + ext.lower() for ext in pathext]
+  else:
+    files = [cmd]
+  seen = set()
+  for dir in path:
+    dir = os.path.normcase(dir)
+    if dir not in seen:
+      seen.add(dir)
+      for thefile in files:
+        name = os.path.join(dir, thefile)
+        if _access_check(name, mode):
+          return name
+  return None
 
 
 class X(install):
@@ -14,34 +43,34 @@ class X(install):
     result = False
     ext = ".exe" if sys.platform.startswith("win") else ""
     nimble_exe = 'nimble' + ext  # Try "nimble"
-    if subprocess.run(f"{ nimble_exe } --version", shell=True, timeout=99).returncode != 0:
-      nimble_exe = pathlib.Path.home() / '.nimble' / 'bin' / f"nimble{ext}"  # Try full path to "nimble"
-      if subprocess.run(f"{ nimble_exe } --version", shell=True, timeout=99).returncode != 0:
-        warnings.warn(f"Nimble not found, tried '{ nimble_exe }' and 'nimble'")
-    nim_exe = shutil.which(f"nim{ext}")  # Ask shutil for "nim"
+    if subprocess.call(nimble_exe + " --version", shell=True, timeout=99) != 0:
+      nimble_exe = os.path.join(home, '.nimble', 'bin', "nimble" + ext)  # Try full path to "nimble"
+      if subprocess.call(nimble_exe + " --version", shell=True, timeout=99) != 0:
+        warnings.warn("Nimble not found, tried 'nimble' and " + nimble_exe)
+    nim_exe = which("nim" + ext)  # Ask which for "nim"
     if nim_exe is not None:
-      nim_exe = pathlib.Path(nim_exe)
-    if subprocess.run(f"{ nim_exe } --version", shell=True, timeout=99).returncode != 0:
-      nim_exe = pathlib.Path.home() / '.nimble' / 'bin' / f"nim{ext}"  # Try full path to "nim"
-      if subprocess.run(f"{ nim_exe } --version", shell=True, timeout=99).returncode != 0:
-        warnings.warn(f"Nim not found, tried '{ nim_exe }' and 'nim'")
+      nim_exe = str(nim_exe)
+    if subprocess.call(nim_exe + " --version", shell=True, timeout=99) != 0:
+      nim_exe = os.path.join(home, '.nimble', 'bin', "nim" + ext)  # Try full path to "nim"
+      if subprocess.call(nim_exe + " --version", shell=True, timeout=99) != 0:
+        warnings.warn("Nim not found, tried 'nim' and " + nim_exe)
     if os.path.exists(nimble_exe):
-      nimble_cmd = f"{ nimble_exe } -y --noColor --nim:'{ nim_exe }'"
-      if subprocess.run(f"{ nimble_cmd } refresh", shell=True, timeout=999).returncode == 0:
-        print(f"OK\t{ nimble_cmd } --verbose refresh")
-        if subprocess.run(f"{ nimble_cmd } install nimpy", shell=True, timeout=999).returncode == 0:
-          print(f"OK\t{ nimble_cmd } install nimpy")
-          if subprocess.run(f"{ nimble_cmd } install fusion", shell=True, timeout=999).returncode == 0:
-            print(f"OK\t{ nimble_cmd } install fusion")
+      nimble_cmd = nimble_exe + " -y --noColor --nim:'" + nim_exe + "'"
+      if subprocess.call(nimble_cmd + " refresh", shell=True, timeout=999) == 0:
+        print("OK\t" + nimble_cmd + " --verbose refresh")
+        if subprocess.call(nimble_cmd + " install nimpy", shell=True, timeout=999) == 0:
+          print("OK\t" + nimble_cmd + " install nimpy")
+          if subprocess.call(nimble_cmd + " install fusion", shell=True, timeout=999) == 0:
+            print("OK\t" + nimble_cmd + " install fusion")
             result = True
           else:
-            warnings.warn(f"Failed to run '{ nimble_cmd } install fusion'")
+            warnings.warn("Failed to run '" + nimble_cmd + " install fusion'")
         else:
-          warnings.warn(f"Failed to run '{ nimble_cmd } install nimpy'")
+          warnings.warn("Failed to run '" + nimble_cmd + " install nimpy'")
       else:
-        warnings.warn(f"Failed to run '{ nimble_cmd } refresh'")
+        warnings.warn("Failed to run '" + nimble_cmd + " refresh'")
     else:
-      warnings.warn(f"File not found '{ nimble_exe }'")
+      warnings.warn("File not found " + nimble_exe)
     return result
 
   def choosenim_setup(self):
@@ -50,37 +79,39 @@ class X(install):
     # if it is not installed run "init.sh" or "choosenim --firstInstall" to install choosenim.
     result = False
     choosenim_exe = "choosenim.exe" if sys.platform.startswith("win") else "choosenim"
-    if subprocess.run(f"{ choosenim_exe } --version", shell=True, timeout=999).returncode == 0:
-      warnings.warn(f"Choosenim is already installed and working on the system '{ choosenim_exe }'")
-      if subprocess.run(f"{ choosenim_exe } update self", shell=True, timeout=999).returncode != 0:
-        warnings.warn(f"Failed to run '{ choosenim_exe } update self'")  # Dont worry if "update self" fails.
-      if subprocess.run(f"{ choosenim_exe } update stable", shell=True, timeout=999).returncode == 0:
+    if subprocess.call(choosenim_exe + " --version", shell=True, timeout=999) == 0:
+      warnings.warn("Choosenim is already installed and working on the system " + choosenim_exe)
+      if subprocess.call(choosenim_exe + " update self", shell=True, timeout=999) != 0:
+        warnings.warn("Failed to run '" + choosenim_exe + " update self'")  # Dont worry if "update self" fails.
+      if subprocess.call(choosenim_exe + " update stable", shell=True, timeout=999) == 0:
         result = True
       else:
-        warnings.warn(f"Failed to run '{ choosenim_exe } update stable'")
+        warnings.warn("Failed to run '" + choosenim_exe + " update stable'")
     else:
-      choosenim_exe = pathlib.Path(__file__).parent / "choosenim.exe" if sys.platform.startswith("win") else "init.sh"
+      choosenim_exe = os.path.join(os.path.dirname(__file__), "choosenim.exe") if sys.platform.startswith("win") else "init.sh"
       if os.path.exists(choosenim_exe):
-        choosenim_cmd = f"{ '' if sys.platform.startswith('win') else 'sh '}{ choosenim_exe } { ' --yes --verbose --noColor --firstInstall stable' if sys.platform.startswith('win') else ' -y' }"
-        if subprocess.run(choosenim_cmd, shell=True, timeout=999).returncode == 0:
-          print(f"OK\t{ choosenim_cmd }")
+        exe_prefix = '' if sys.platform.startswith('win') else 'sh '
+        exe_params = ' --yes --verbose --noColor --firstInstall stable' if sys.platform.startswith('win') else ' -y'
+        choosenim_cmd = exe_prefix + choosenim_exe + exe_params
+        if subprocess.call(choosenim_cmd, shell=True, timeout=999) == 0:
+          print("OK\t" + choosenim_cmd)
           if sys.platform.startswith('win'):
-            if subprocess.run(f"{ choosenim_exe } stable --firstInstall", shell=True, timeout=999).returncode == 0:
+            if subprocess.call(choosenim_exe + " stable --firstInstall", shell=True, timeout=999) == 0:
               result = True
           else:
             result = True
         else:
-          warnings.warn(f"Failed to run '{ choosenim_cmd }'")
+          warnings.warn("Failed to run " + choosenim_cmd)
       else:
-        warnings.warn(f"File not found '{ choosenim_exe }'")
-      shutil.rmtree(str(pathlib.Path.home() / ".choosenim" / "downloads"), ignore_errors=True)  # Clear download cache.
+        warnings.warn("File not found " + choosenim_exe)
+      shutil.rmtree(os.path.join(home, ".choosenim", "downloads"), ignore_errors=True)  # Clear download cache.
     return result
 
   def add_to_path(self):
     # On Linux add Nim to the PATH.
     if not sys.platform.startswith("win"):
-      new_path = f"export PATH={ pathlib.Path.home() / '.nimble/bin' }:$PATH"
-      filename = pathlib.Path.home() / ".bashrc"
+      new_path = "export PATH=" + os.path.join(home, '.nimble/bin') + ":$PATH"
+      filename = os.path.join(home, ".bashrc")
       try:
         if filename.exists():
           found = False
@@ -93,10 +124,10 @@ class X(install):
         else:
           with open(filename, "w") as f:
             f.write(new_path)
-        print(f"OK\t{ filename }")
+        print("OK\t" + filename)
       except:
-        warnings.warn(f"Failed to write file: {filename}")
-      filename = pathlib.Path.home() / ".profile"
+        warnings.warn("Failed to write file: " + filename)
+      filename = os.path.join(home, ".profile")
       try:
         if filename.exists():
           found = False
@@ -109,10 +140,10 @@ class X(install):
         else:
           with open(filename, "w") as f:
             f.write(new_path)
-        print(f"OK\t{ filename }")
+        print("OK\t" + filename)
       except:
         warnings.warn("Failed to write file ~/.profile")
-      filename = pathlib.Path.home() / ".bash_profile"
+      filename = os.path.join(home, ".bash_profile")
       try:
         if filename.exists():
           found = False
@@ -125,10 +156,10 @@ class X(install):
         else:
           with open(filename, "w") as f:
             f.write(new_path)
-        print(f"OK\t{ filename }")
+        print("OK\t" + filename)
       except:
         warnings.warn("Failed to write file ~/.bash_profile")
-      filename = pathlib.Path.home() / ".zshrc"
+      filename = os.path.join(home, ".zshrc")
       try:
         if filename.exists():
           found = False
@@ -141,11 +172,11 @@ class X(install):
         else:
           with open(filename, "w") as f:
             f.write(new_path)
-        print(f"OK\t{ filename }")
+        print("OK\t" + filename)
       except:
         warnings.warn("Failed to write file ~/.zshrc")
       # https://github.com/juancarlospaco/choosenim_install/issues/4
-      filename = pathlib.Path.home() / ".zshenv"
+      filename = os.path.join(home, ".zshenv")
       try:
         if filename.exists():
           found = False
@@ -158,7 +189,7 @@ class X(install):
         else:
           with open(filename, "w") as f:
             f.write(new_path)
-        print(f"OK\t{ filename }")
+        print("OK\t" + filename)
       except:
         warnings.warn("Failed to write file ~/.zshenv")
 
