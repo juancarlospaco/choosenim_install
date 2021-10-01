5fc126fbe8496d63039ef4322d4df1c3714fe5f9
Juan Carlos <juancarlospaco@gmail.com>
2021-09-30 19:45:16 -0700

    Re-implement choosenim functionalities in pure Python

--- dist/setup.py
+++ dist/setup.py
@@ -241,7 +241,7 @@ def nimble_setup():
       if subprocess.call(nim_exe + " --version", shell=True, timeout=99) != 0:
         warnings.warn("Nim not found, tried 'nim' and " + nim_exe)
   if os.path.exists(nimble_exe):
-    os.environ["PATH"] = os.path.join(home, ".choosenim", "toolchains", "nim-#devel", "bin") + ":$PATH"
+    os.environ["PATH"] = "$PATH:" + os.path.join(home, ".choosenim", "toolchains", "nim-#devel", "bin")
     nimble_cmd = nimble_exe + " --accept --noColor --noSSLCheck --nim:" + nim_exe
     if subprocess.call(nimble_cmd + " refresh", shell=True, timeout=999) == 0:
       print("OK\t" + nimble_cmd + " --verbose refresh")
