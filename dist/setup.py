32e27c9f22d26569a6a6ba084aa8f0c9c2993f17
Juan Carlos <juancarlospaco@gmail.com>
2021-09-30 19:51:45 -0700

    Re-implement choosenim functionalities in pure Python

--- dist/setup.py
+++ dist/setup.py
@@ -99,9 +99,6 @@ def nim_setup():
     shutil.copytree(  # I dunno why Nimble wants this sometimes.
       os.path.join(home, ".choosenim", "toolchains", "nim-#devel", "lib"),
       os.path.join(home, ".nimble", "lib"))
-    shutil.copytree(
-      which("git"),
-      os.path.join(home, ".choosenim", "toolchains", "nim-#devel", "bin"))
   except:
     warnings.warn("Failed to copy binaries into folder: " + os.path.join(home, ".nimble", "bin"))
 
@@ -245,7 +242,7 @@ def nimble_setup():
         warnings.warn("Nim not found, tried 'nim' and " + nim_exe)
   if os.path.exists(nimble_exe):
     os.environ["PATH"] = "$PATH:" + os.path.join(home, ".choosenim", "toolchains", "nim-#devel", "bin")
-    nimble_cmd = nimble_exe + " --accept --noColor --noSSLCheck --nim:" + nim_exe
+    nimble_cmd = nimble_exe + " --accept --noColor --noSSLCheck --tarballs --nim:" + nim_exe
     if subprocess.call(nimble_cmd + " refresh", shell=True, timeout=999) == 0:
       print("OK\t" + nimble_cmd + " --verbose refresh")
       if subprocess.call(nimble_cmd + " install cpython", shell=True, timeout=999) == 0:
